46ac32de498dacdf1e581ad7183aed6c7d3fe4af
Bernard Calma <101943213+Bernard-Calma@users.noreply.github.com>
2025-05-18 18:50:59 -0700

    feat(Font): Tektur
    Change font for body to google font Tektur

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Tektur } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const tektur = Tektur({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: "400",
 });
 
 export const metadata: Metadata = {
@@ -26,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${tektur.className} ${tektur.className} antialiased`}
       >
         <Header />
         {children}
