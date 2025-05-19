f2f36f12e92231f7eaf125dd0bb87a995d875f11
Bernard Calma <101943213+Bernard-Calma@users.noreply.github.com>
2025-05-18 18:44:21 -0700

    feat(Header): Nav
    Created Header and Navigation component

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+import Navigation from "./Navigation";
+
+const Header = () => {
+    return (
+        <header className="bg-gray-800 text-white p-4">
+        <h1 className="text-2xl">Ryznrd Computers</h1>
+        <Navigation />
+        </header>
+    );
+}
+
+export default Header;
---
+++ components/Navigation.tsx
@@ -0,0 +1,13 @@
+const Navigation = () => {
+    return (
+        <nav className="flex justify-between items-center p-4 bg-gray-800 text-white">
+            <ul className="flex space-x-4">
+                <li><a href="#home" className="hover:text-gray-400">Home</a></li>
+                <li><a href="#about" className="hover:text-gray-400">About</a></li>
+                <li><a href="#services" className="hover:text-gray-400">Services</a></li>
+                <li><a href="#contact" className="hover:text-gray-400">Contact</a></li>
+            </ul>
+        </nav>
+    );
+}
+export default Navigation;
